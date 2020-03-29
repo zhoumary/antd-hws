266a09f041a1ed4e29cb836fba09a1f6f04250da
I323799 <fiona.zhou01@sap.com>
2020-03-30 00:01:04 +0800

    enhance redux thunk

--- src/redux-ts/store.ts
+++ src/redux-ts/store.ts
@@ -1,8 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
-// import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import rootReducer from "./reducers-ts";
 
-// const middlewares = [thunk];
+const middlewares = [thunk];
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, applyMiddleware(...middlewares));
